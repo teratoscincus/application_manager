923e59a399a979c83fd43e7f7c1d7e16f01c93e0
Max <teratoscincus@hotmail.com>
2023-05-23 11:36:56 +0200

    Update views

--- applications/views.py
+++ applications/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, CreateView, UpdateView
+from django.db.models import Q
+from django.views.generic import CreateView, ListView, UpdateView
 
-from .models import Application
+from .models import AdmissionTest, Application
 
 
 class ListApplicationsView(LoginRequiredMixin, ListView):
@@ -9,20 +10,83 @@ class ListApplicationsView(LoginRequiredMixin, ListView):
     context_object_name = "applications"
 
     def get_queryset(self):
-        return Application.objects.filter(user=self.request.user).order_by("deadline")
+        not_applied = (
+            Application.objects.filter(user=self.request.user)
+            .filter(has_applied=False)
+            .order_by("deadline")
+        )
+        has_applied = (
+            Application.objects.filter(user=self.request.user)
+            .filter(has_applied=True)
+            .order_by("title")
+            .order_by("institute__name")
+        )
+
+        return {"not_applied": not_applied, "has_applied": has_applied}
 
 
 class CreateApplicationView(LoginRequiredMixin, CreateView):
     model = Application
-    fields = ["title", "school", "has_applied", "deadline", "url"]
+    fields = [
+        "has_applied",
+        "title",
+        "institute",
+        "deadline",
+        "url",
+    ]
 
-    template_name = "applications/new_application.html"
+    template_name = "applications/new_course_application.html"
     context_object_name = "form"
+    success_url = "/applications/"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 
 class UpdateApplicationView(LoginRequiredMixin, UpdateView):
     model = Application
-    fields = ["title", "school", "has_applied", "deadline", "url"]
+    fields = [
+        "has_applied",
+        "title",
+        "institute",
+        "deadline",
+        "url",
+    ]
 
-    template_name = "applications/edit_application.html"
+    template_name = "applications/edit_course_application.html"
     context_object_name = "form"
+    success_url = "/applications/"
+
+
+class AdmissionTestsView(LoginRequiredMixin, ListView):
+    template_name = "applications/admission_tests.html"
+    context_object_name = "admission_tests"
+
+    def get_queryset(self):
+        """Separate admisson tests where the start date is TBA."""
+        tba = AdmissionTest.objects.filter(start=None).order_by(
+            "application__institute__name"
+        )
+        not_done = (
+            AdmissionTest.objects.filter(~Q(start=None))
+            .filter(done=False)
+            .order_by("start")
+        )
+        is_done = AdmissionTest.objects.filter(done=True).order_by("-start")
+
+        return {"tba": tba, "not_done": not_done, "is_done": is_done}
+
+
+class UpdateAdmissionTestView(LoginRequiredMixin, UpdateView):
+    model = AdmissionTest
+    fields = [
+        "done",
+        "start",
+        "deadline",
+        "notes",
+    ]
+
+    template_name = "applications/edit_admission_test.html"
+    context_object_name = "form"
+    success_url = "/applications/admission-tests/"
